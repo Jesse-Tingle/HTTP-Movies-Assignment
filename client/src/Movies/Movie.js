4556e82e2a56df81f5f0b26a12eeefff5db2fb97
Jesse-Tingle <jtingle0@gmail.com>
2019-11-08 05:40:42 -0500

    converted from class components to Hooks

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,48 +1,47 @@
-import React from "react";
-import axios from "axios";
-import MovieCard from "./MovieCard";
-export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MovieCard from './MovieCard';
+
+const Movie = (props) => {
+  const [movie, setMovie] = useState(null);
+ console.log('Movies', props.match.params.id);
+ 
+ const id = props.match.params.id;
+    // change ^^^ that line and grab the id from the URL
+    // You will NEED to add a dependency array to this effect hook
+ 
+ useEffect(() => {
+    
+
+       axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then(response => {
+          setMovie(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+  },[id]);
+  
+  // Uncomment this only when you have moved on to the stretch goals
+  const saveMovie = () => {
+    console.log('saveMovieProps', props);
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
   }
 
-  componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
+  if (!movie) {
+    return <div>Loading movie information...</div>;
   }
 
-  componentWillReceiveProps(newProps) {
-    if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
-    }
-  }
-
-  fetchMovie = id => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => this.setState({ movie: res.data }))
-      .catch(err => console.log(err.response));
-  };
-
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
-  };
-
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
-
-    return (
-      <div className="save-wrapper">
-        <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
-          Save
-        </div>
-      </div>
-    );
-  }
+  
+  return (
+    <div className="save-wrapper">
+      <MovieCard movie={movie}/>
+      <div onClick={saveMovie} className="save-button">Save</div>
+    </div>
+  );
 }
+
+export default Movie;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,37 +1,43 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import MovieCard from "./MovieCard";
-export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-  componentDidMount() {
-    axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => this.setState({ movies: res.data }))
-      .catch(err => console.log(err.response));
-  }
+import { Link } from 'react-router-dom';
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
+import MovieCard from './MovieCard';
+
+const MovieList = props => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
+  
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 }
 
 function MovieDetails({ movie }) {
+  
   return (
     <Link to={`/movies/${movie.id}`}>
-      <MovieCard movie={movie} />
+      <MovieCard movie={movie}/>
     </Link>
   );
 }
+
+export default MovieList;
